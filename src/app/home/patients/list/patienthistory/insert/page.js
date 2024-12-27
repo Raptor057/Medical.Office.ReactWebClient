77b2ed20c51770211377fb983cb50a4cb476e1a6
Rogelio Arriaga Gonzalez <rogelioarriaga@live.com.mx>
2024-12-27 01:00:50 -0600

    Update and Fix error

--- src/app/home/patients/list/patienthistory/insert/page.js
+++ src/app/home/patients/list/patienthistory/insert/page.js
@@ -1,12 +1,16 @@
 'use client';
 
-import PatientInsertForms from "@/app/components/Forms/Posts/PatientInsertForms";
+import dynamic from "next/dynamic";
 
+const PatientFormsDashboard = dynamic(
+  () => import("@/app/components/Forms/Posts/PatientFormsDashboard"),
+  { ssr: false }
+);
 
 export default function Page() {
-    return (
-      <div className="min-h-screen p-6 bg-gray-100">
-        <PatientInsertForms />
-      </div>
-    );
-  };
+  return (
+    <div className="min-h-screen p-6 bg-gray-100">
+      <PatientFormsDashboard />
+    </div>
+  );
+}
